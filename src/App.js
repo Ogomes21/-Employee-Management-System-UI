a369480dc54ec3d2056249a0bc9f234c19686f27
octavia <Ogomes21@gmail.com>
2022-09-16 14:13:07 -0400

    Added first name, last name, and email inputs. Added Save button.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AddEmployee from "./components/AddEmployee";
 
 //function that returns the entire HTML
 //allows you to write the HTML components within JavaScript
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <AddEmployee />
+    </>
+  );
 }
 
 export default App;
--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
-function AddEmployee() {
+const AddEmployee = () => {
   return (
-    <div>AddEmployee</div>
-  )
-}
+    <div className="flex max-w-2xl mx-auto shadow border-b">
+      <div className="px-8 py-8">
+        <div className="font-thin text-2xl tracking-wider">
+          <h1>Add New Employee</h1>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            First Name
+          </label>
+          <input
+            type="text"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            Email
+          </label>
+          <input
+            type="email"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+            <button className="rounded text-white font-semibold bg-pink-400 hover:bg-pink-600 py-2 px-6 ">
+                Save
+                </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AddEmployee
+export default AddEmployee;
