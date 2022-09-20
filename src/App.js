376bc280ef7a831fe07cffca0349c7deed65e328
octavia <Ogomes21@gmail.com>
2022-09-20 13:21:46 -0400

    Added EmployeeList.js and created BrowserRouter in App.js.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import AddEmployee from "./components/AddEmployee";
+import { BrowserRouter } from "react-router-dom";
+import EmployeeList from "./components/EmployeeList";
 
 //function that returns the entire HTML
 //allows you to write the HTML components within JavaScript
 function App() {
   return (
     <>
-      <Navbar />
-      <AddEmployee />
+    <BrowserRouter>
+    <Navbar />
+    <AddEmployee />
+    <EmployeeList />
+    </BrowserRouter>
     </>
   );
 }
